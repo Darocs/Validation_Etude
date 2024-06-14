6c9dbcba00e606f66a1b448ed4c0ec365f273ca9
Darocs <darocs@mail.ru>
2024-06-14 13:17:56 +0300

    Added Res for ErrorToMessage

--- app/src/main/java/ru/polescanner/validation_etude/ui/reusable/util/ErrorToMessage.kt
+++ app/src/main/java/ru/polescanner/validation_etude/ui/reusable/util/ErrorToMessage.kt
@@ -1,7 +1,6 @@
 package ru.polescanner.validation_etude.ui.reusable.util
 
 import ru.polescanner.validation_etude.R
-import ru.polescanner.validation_etude.domain.general.ConductorCount
 import ru.polescanner.validation_etude.domain.general.ErrorType
 import ru.polescanner.validation_etude.domain.general.Name
 import ru.polescanner.validation_etude.domain.general.Name.ValidationError.ExceedMaxChar
@@ -11,33 +10,36 @@ import ru.polescanner.validation_etude.domain.general.Name.ValidationError.Viola
 
 fun <E : ErrorType> E.errorToMessage(): UiText =
     when (this) {
-        NotTrimmed -> TODO()
+        NotTrimmed -> UiText.Res(R.string.not_trimmed)
+
         is ExceedMaxChar -> UiText.Res(R.string.is_too_long) + UiText.Str(" ${this.max} ") + UiText.Res(
-            R.string.app_name
-        ) // TODO: Add res
+            R.string.chars
+        )
 
         is ShortMinChar -> UiText.Res(R.string.not_enough_chars) + UiText.Str(" ${this.min} ") + UiText.Res(
-            R.string.app_name
-        ) // TODO: Add res
+            R.string.chars
+        )
 
-        is ViolationAllowedChars -> TODO()
+        is ViolationAllowedChars -> UiText.Res(R.string.not_allowed_chars) + UiText.Str(": ${this.regex}")
 
-        else -> TODO()
+        else -> UiText.Res(R.string.smth_get_wrong)
     }
 
 fun Name.ValidationError.toMessage(): UiText =
     when (this) {
-        NotTrimmed -> TODO()
+        NotTrimmed -> UiText.Res(R.string.not_trimmed)
+
         is ExceedMaxChar -> UiText.Res(R.string.is_too_long) + UiText.Str(" ${this.max} ") + UiText.Res(
-            R.string.app_name
-        ) // TODO: Add res
+            R.string.chars
+        )
 
         is ShortMinChar -> UiText.Res(R.string.not_enough_chars) + UiText.Str(" ${this.min} ") + UiText.Res(
-            R.string.app_name
-        ) // TODO: Add res
+            R.string.chars
+        )
 
-        is ViolationAllowedChars -> TODO()
-        ConductorCount.ValidationError.Negative -> TODO()
+        is ViolationAllowedChars -> UiText.Res(R.string.not_allowed_chars) + UiText.Str(": ${this.regex}")
+
+        else -> UiText.Res(R.string.smth_get_wrong)
     }
 
 fun ErrorType.toMessage(): UiText = when(this) {
