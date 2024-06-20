7ef9e07a07998caa0966193b7173cc4a7b2fea1b
Darocs <darocs@mail.ru>
2024-06-20 10:41:52 +0300

    Test: new feature `assertStateDescriptionContains`

--- app/src/androidTest/java/ru/polescanner/validation_etude/ui/signin/extensions/SignInTestExtensions.kt
+++ app/src/androidTest/java/ru/polescanner/validation_etude/ui/signin/extensions/SignInTestExtensions.kt
@@ -3,6 +3,7 @@ package ru.polescanner.validation_etude.ui.signin.extensions
 import com.atiurin.ultron.core.compose.nodeinteraction.UltronComposeSemanticsNodeInteraction
 import com.atiurin.ultron.core.compose.nodeinteraction.clickCenterRight
 import ru.polescanner.validation_etude.domain.general.DI
+import ru.polescanner.validation_etude.ui.reusable.components.AssertStateDescription.assertStateDescriptionContains
 
 object LoginExtensions {
     fun UCS.loginInvalidMinChars(): UCS = this.setText("1")
@@ -10,26 +11,32 @@ object LoginExtensions {
         .assertTextContains("Min 2 chars")
         .assertTextContains("Login*")
         .assertContentDescriptionContains("invalid")
+        .assertStateDescriptionContains("invalid")
 
     fun UCS.loginInvalidMaxChars(): UCS = this.setText("123456")
         .assertTextContains("123456")
         .assertTextContains("Max 5 chars")
         .assertContentDescriptionContains("invalid")
+        .assertStateDescriptionContains("invalid")
 
     fun UCS.loginInvalidRegex(): UCS = this.setText("A")
         .assertTextContains("A")
         .assertTextContains("Min 2 chars")
         .assertContentDescriptionContains("invalid")
+        .assertStateDescriptionContains("invalid")
         .setText("1B")
         .assertTextContains("Allowed chars: ${DI.login?.regex}")
+        .assertStateDescriptionContains("invalid")
         .setText("ABCDEF")
         .assertTextContains("Max 5 chars")
+        .assertStateDescriptionContains("invalid")
 
     fun UCS.loginIsValid(): UCS = this.setText("12")
         .assertTextContains("12")
         .assertTextContains("e.g. Darocs")
         .assertTextContains("Login")
         .assertContentDescriptionContains("valid")
+        .assertStateDescriptionContains("valid")
 }
 
 object PasswordExtensions {
@@ -40,23 +47,29 @@ object PasswordExtensions {
         .assertTextContains("Min 3 chars")
         .assertTextContains("Password*")
         .assertContentDescriptionContains("invalid")
+        .assertStateDescriptionContains("invalid")
 
     fun UCS.passwordInvalidMaxChars(): UCS = this.setText("ABCDEFG")
         .assertTextContains("Max 6 chars")
         .assertContentDescriptionContains("invalid")
+        .assertStateDescriptionContains("invalid")
 
     fun UCS.passwordInvalidRegex(): UCS = this.setText("1")
         .assertTextContains("Min 3 chars")
         .assertContentDescriptionContains("invalid")
+        .assertStateDescriptionContains("invalid")
         .setText("AB3")
         .assertTextContains("Allowed chars: ${DI.password?.regex}")
+        .assertStateDescriptionContains("invalid")
         .setText("1234567")
         .assertTextContains("Max 6 chars")
+        .assertStateDescriptionContains("invalid")
 
     fun UCS.passwordIsValid(): UCS = this.setText("ABC")
         .assertTextContains("ABC")
         .assertTextContains("Password")
         .assertContentDescriptionContains("valid")
+        .assertStateDescriptionContains("valid")
 }
 
 typealias UCS = UltronComposeSemanticsNodeInteraction
