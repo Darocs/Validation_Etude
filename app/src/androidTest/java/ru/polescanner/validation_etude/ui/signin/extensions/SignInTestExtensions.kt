bdf283927c298748c55bf85e735eb0485daa62d7
Darocs <darocs@mail.ru>
2024-06-21 10:57:52 +0300

    Refactoring: update assertions in SignInTestExtensions.kt

--- app/src/androidTest/java/ru/polescanner/validation_etude/ui/signin/extensions/SignInTestExtensions.kt
+++ app/src/androidTest/java/ru/polescanner/validation_etude/ui/signin/extensions/SignInTestExtensions.kt
@@ -2,30 +2,31 @@ package ru.polescanner.validation_etude.ui.signin.extensions
 
 import com.atiurin.ultron.core.compose.nodeinteraction.UltronComposeSemanticsNodeInteraction
 import com.atiurin.ultron.core.compose.nodeinteraction.clickCenterRight
-import ru.polescanner.validation_etude.domain.general.DI
+import ru.polescanner.validation_etude.domain.general.DI.login
+import ru.polescanner.validation_etude.domain.general.DI.password
 import ru.polescanner.validation_etude.ui.reusable.components.AssertStateDescription.assertStateDescriptionContains
 
 object LoginExtensions {
     fun UCS.loginInvalidMinChars(): UCS = this.setText("1")
         .assertTextContains("1")
-        .assertTextContains("Min 2 chars")
+        .assertTextContains("Min ${login?.min} chars")
         .assertTextContains("Login*")
         .assertStateDescriptionContains("invalid")
 
-    fun UCS.loginInvalidMaxChars(): UCS = this.setText("123456")
-        .assertTextContains("123456")
-        .assertTextContains("Max 5 chars")
+    fun UCS.loginInvalidMaxChars(): UCS = this.setText("1234")
+        .assertTextContains("1234")
+        .assertTextContains("Max ${login?.max} chars")
         .assertStateDescriptionContains("invalid")
 
     fun UCS.loginInvalidRegex(): UCS = this.setText("A")
         .assertTextContains("A")
-        .assertTextContains("Min 2 chars")
+        .assertTextContains("Min ${login?.min} chars")
         .assertStateDescriptionContains("invalid")
         .setText("1B")
-        .assertTextContains("Allowed chars: ${DI.login?.regex}")
+        .assertTextContains("Allowed chars: ${login?.regex}")
         .assertStateDescriptionContains("invalid")
-        .setText("ABCDEF")
-        .assertTextContains("Max 5 chars")
+        .setText("ABCD")
+        .assertTextContains("Max ${login?.max} chars")
         .assertStateDescriptionContains("invalid")
 
     fun UCS.loginIsValid(): UCS = this.setText("12")
@@ -40,22 +41,22 @@ object PasswordExtensions {
         .assertTextContains("••")
         .clickCenterRight()
         .assertTextContains("AB")
-        .assertTextContains("Min 3 chars")
+        .assertTextContains("Min ${password?.min} chars")
         .assertTextContains("Password*")
         .assertStateDescriptionContains("invalid")
 
     fun UCS.passwordInvalidMaxChars(): UCS = this.setText("ABCDEFG")
-        .assertTextContains("Max 6 chars")
+        .assertTextContains("Max ${password?.max} chars")
         .assertStateDescriptionContains("invalid")
 
     fun UCS.passwordInvalidRegex(): UCS = this.setText("1")
-        .assertTextContains("Min 3 chars")
+        .assertTextContains("Min ${password?.min} chars")
         .assertStateDescriptionContains("invalid")
         .setText("AB3")
-        .assertTextContains("Allowed chars: ${DI.password?.regex}")
+        .assertTextContains("Allowed chars: ${password?.regex}")
         .assertStateDescriptionContains("invalid")
         .setText("1234567")
-        .assertTextContains("Max 6 chars")
+        .assertTextContains("Max ${password?.max} chars")
         .assertStateDescriptionContains("invalid")
 
     fun UCS.passwordIsValid(): UCS = this.setText("ABC")
