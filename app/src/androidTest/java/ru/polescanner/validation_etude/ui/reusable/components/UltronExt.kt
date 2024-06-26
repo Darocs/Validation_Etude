7627a8397bbc26629df4767cd1e7e2ee0f7a8e10
Darocs <darocs@mail.ru>
2024-06-26 16:38:55 +0300

    Feature: add `tapValidChar` to UltronExt.kt

--- app/src/androidTest/java/ru/polescanner/validation_etude/ui/reusable/components/UltronExt.kt
+++ app/src/androidTest/java/ru/polescanner/validation_etude/ui/reusable/components/UltronExt.kt
@@ -2,9 +2,13 @@ package ru.polescanner.validation_etude.ui.reusable.components
 
 import androidx.compose.ui.semantics.getOrNull
 import com.atiurin.ultron.core.compose.nodeinteraction.clickCenterRight
+import ru.polescanner.validation_etude.ui.signin.extensions.Config
 import ru.polescanner.validation_etude.ui.signin.extensions.UCS
+import ru.polescanner.validation_etude.ui.signin.extensions.times
 
 object TextField {
+    val config: Config = Config()
+
     fun UCS.getEditableText() : String? {
         return this.getNode().config.getOrNull(androidx.compose.ui.semantics.SemanticsProperties.EditableText)?.text
     }
@@ -15,4 +19,7 @@ object TextField {
         val text = this.getEditableText() ?: ""
         return this.setText(text.dropLast(times))
     }
+
+    fun UCS.tapValidChar(times: Int = 1) : UCS = this
+        .typeText(config.validChar.times(times))
 }
